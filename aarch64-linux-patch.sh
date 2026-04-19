496a8fe642e5ecf1c4fdc483c1c004005e61d833
UserAcmo10086 <acmo10086@gmail.com>
2026-04-19 19:45:27 +0800

    Update aarch64-linux-patch.sh

--- aarch64-linux-patch.sh
+++ aarch64-linux-patch.sh
@@ -46,13 +46,20 @@ else
     echo "警告：$TARGET_FILE 不存在，跳过修复。"
 fi
 
-
-# 修复 native_handle.cpp 中的 android_fdsan_create_owner_tag 参数类型错误
+# 修复 native_handle.cpp 中的 fdsan 类型转换
 NATIVE_HANDLE_FILE="submodules/core/libcutils/native_handle.cpp"
 if [[ -f "$NATIVE_HANDLE_FILE" ]]; then
     echo ">>> 修复 $NATIVE_HANDLE_FILE 中的 fdsan 类型转换"
     sed -i 's/android_fdsan_create_owner_tag(ANDROID_FDSAN_OWNER_TYPE_NATIVE_HANDLE,/android_fdsan_create_owner_tag((enum android_fdsan_owner_type)ANDROID_FDSAN_OWNER_TYPE_NATIVE_HANDLE,/' "$NATIVE_HANDLE_FILE"
 fi
+
+# 为 canned_fs_config.cpp 和 fs_config.cpp 添加空宏定义以绕过 GNU 扩展语法错误
+for f in "submodules/core/libcutils/canned_fs_config.cpp" "submodules/core/libcutils/fs_config.cpp"; do
+    if [[ -f "$f" ]]; then
+        echo ">>> 修复 $f 的 GNU 扩展冲突"
+        sed -i '1i #define __THROW\n#define __wur' "$f"
+    fi
+done
 
 # 创建符号链接
 ln -sf "../../googletest" "submodules/boringssl/src/third_party/googletest"
