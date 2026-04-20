2f95f1b0acd519d6c9d895bc461559eff58246fc
UserAcmo10086 <acmo10086@gmail.com>
2026-04-20 10:22:54 +0800

    Update aarch64-linux-build.sh

--- aarch64-linux-build.sh
+++ aarch64-linux-build.sh
@@ -53,12 +53,12 @@ export LIBRARY_PATH="${CRTBEGIN_T_DIR}:${LINUX_SYSROOT}/lib:${LINUX_SYSROOT}/usr
 COMMON_FLAGS="--target=aarch64-linux-gnu --sysroot=${LINUX_SYSROOT} --gcc-toolchain=/usr"
 COMMON_FLAGS+=" -fPIC -Wno-attributes -fcolor-diagnostics"
 CFLAGS="${COMMON_FLAGS} -std=gnu11"
+# 全局 CXXFLAGS 不包含任何 NDK 路径，保证其他目标不受干扰
 CXXFLAGS="${COMMON_FLAGS} -std=gnu++17"
 CXXFLAGS+=" -D__GLIBC_PREREQ\(x,y\)=1 -D__GNUC_PREREQ\(x,y\)=1 -D__GLIBC_USE\(x\)=1"
 CXXFLAGS+=" -D__THROW= -D__wur= -D__nonnull\(x\)="
 CXXFLAGS+=" -include limits -include cstring"
 CXXFLAGS+=" -isystem ${CXX_TOP_DIR} -isystem ${CXX_ARCH_DIR}"
-CXXFLAGS+=" -isystem ${NDK_SYSROOT}/usr/include"
 
 LINKER_FLAGS="-fuse-ld=lld -static -L${CRTBEGIN_T_DIR} -L${LINUX_SYSROOT}/lib -L${LINUX_SYSROOT}/usr/lib -lstdc++"
 
